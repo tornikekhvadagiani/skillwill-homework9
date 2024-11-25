46034afb8b00258cf190dd6b11e8b0659b82a27b
tornikekhvadagiani <tornikexvadagiani1@gmail.com>
2024-11-25 13:13:19 +0400

    first commit

---
+++ main.js
@@ -0,0 +1,13 @@
+// ● მოახდინე პროექტის ინიციალიზაცია
+// ● დააინსტალირე პაკეტი validator
+// ● მოიძიე  -ის დოკუმენტაცია
+// npmjs.com - ზე
+// ● დოკუმენტაციის მიხედვით გამოიყენე
+// ბრძანება isEmail და დაბეჭდე არის თუ არა
+// შემდეგი სიტყვები მეილი:
+// ● test@test.com, abcDE123
+
+var validator = require("validator");
+
+console.log(validator.isEmail("test@test.com"));//true
+console.log(validator.isEmail("abcDE123"));//false
